511fc84e85eef44d62e423646f0091deb021d1dc
martin-selise <hasnat.martin@selisegroup.com>
2025-09-17 23:55:12 +0600

    all tours show

--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -54,15 +54,6 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
   type __Unused = __Check
 }
 
-// Validate ../../src/app/tours/page.tsx
-{
-  type __IsExpected<Specific extends AppPageConfig<"/tours">> = Specific
-  const handler = {} as typeof import("../../src/app/tours/page.js")
-  type __Check = __IsExpected<typeof handler>
-  // @ts-ignore
-  type __Unused = __Check
-}
-
 
 
 
